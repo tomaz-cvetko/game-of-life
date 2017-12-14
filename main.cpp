976f4921df18e231747cbe8785d30ccf8f11ad7b
Tomaz Cvetko <tomaz.cvetko.judo@gmail.com>
2017-12-14 22:14:15 +0100

    First update.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,238 @@
+/*This source code copyrighted by Lazy Foo' Productions (2004-2015)
+and may not be redistributed without written permission.*/
+
+//Using SDL, SDL_image, standard IO, math, and strings
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <string>
+#include <cmath>
 #include <iostream>
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+#include "board.h"
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 960;
+// const int SCREEN_WIDTH = 640;
+// const int SCREEN_HEIGHT = 480;
+
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+//Loads individual image as texture
+SDL_Texture* loadTexture( std::string path );
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
+
+bool init()
+{
+    //Initialization flag
+    bool success = true;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+        success = false;
+    }
+    else
+    {
+        //Set texture filtering to linear
+        if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+        {
+            printf( "Warning: Linear texture filtering not enabled!" );
+        }
+
+        //Create window
+        gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL )
+        {
+            printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+            success = false;
+        }
+        else
+        {
+            //Create renderer for window
+            gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
+            if( gRenderer == NULL )
+            {
+                printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+                success = false;
+            }
+            else
+            {
+                //Initialize renderer color
+                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+                //Initialize PNG loading
+                int imgFlags = IMG_INIT_PNG;
+                if( !( IMG_Init( imgFlags ) & imgFlags ) )
+                {
+                    printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+                    success = false;
+                }
+            }
+        }
+    }
+
+    return success;
+}
+
+bool loadMedia()
+{
+    //Loading success flag
+    bool success = true;
+
+    //Nothing to load
+    return success;
+}
+
+void close()
+{
+    //Destroy window	
+    SDL_DestroyRenderer( gRenderer );
+    SDL_DestroyWindow( gWindow );
+    gWindow = NULL;
+    gRenderer = NULL;
+
+    //Quit SDL subsystems
+    IMG_Quit();
+    SDL_Quit();
+}
+
+SDL_Texture* loadTexture( std::string path )
+{
+    //The final texture
+    SDL_Texture* newTexture = NULL;
+
+    //Load image at specified path
+    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+    if( loadedSurface == NULL )
+    {
+        printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+    }
+    else
+    {
+        //Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+        if( newTexture == NULL )
+        {
+            printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+        }
+
+        //Get rid of old loaded surface
+        SDL_FreeSurface( loadedSurface );
+    }
+
+    return newTexture;
+}
+
+int main( int argc, char* args[] )
+{
+    //Start up SDL and create window
+    if( !init() )
+    {
+        printf( "Failed to initialize!\n" );
+    }
+    else
+    {
+        //Load media
+        if( !loadMedia() )
+        {
+            printf( "Failed to load media!\n" );
+        }
+        else
+        {	
+            //Main loop flag
+            bool quit = false;
+            bool pause = false;
+
+            //Event handler
+            SDL_Event e;
+            
+            GameOfLife gameBoard(SCREEN_WIDTH, SCREEN_HEIGHT, 10);
+            gameBoard.populate(1000);
+            //gameBoard.populate(SCREEN_WIDTH*SCREEN_HEIGHT/16);
+
+            //While application is running
+            while( !quit )
+            {
+                //Handle events on queue
+                while( SDL_PollEvent( &e ) != 0 )
+                {
+                    //User requests quit
+                    if( e.type == SDL_QUIT )
+                    {
+                        quit = true;
+                    }
+                    else if( e.type == SDL_KEYDOWN)
+                    {
+                        if(e.key.keysym.sym == 'w' && e.key.keysym.mod & KMOD_LCTRL)
+                        {
+                            quit = true;
+                        }
+                        else if(e.key.keysym.sym == 'p')
+                        {
+                            pause = (pause) ? false:true;
+                        }
+                        else if(pause && e.key.keysym.sym == 'e')
+                        {
+                            bool edit = true;
+                            while(edit)
+                            {
+                                SDL_PollEvent( &e );
+                                if(e.type == SDL_KEYDOWN && e.key.keysym.sym == 'e')
+                                {
+                                    edit = false;
+                                }
+                                else if(e.type == SDL_MOUSEBUTTONUP)
+                                {
+                                    gameBoard.change(e.button.x, e.button.y);
+                                    
+                                    SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+                                    SDL_RenderClear( gRenderer );
+                                    
+                                    gameBoard.draw(gRenderer);
+                                    
+                                    SDL_RenderPresent( gRenderer );
+                                    SDL_Delay(200);
+                                    std::cout << e.button.x <<' ' << e.button.y <<std::endl;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if(!pause)
+                {
+                    //Clear screen
+                    SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+                    SDL_RenderClear( gRenderer );
+                    
+                    gameBoard.next_generation();
+                    gameBoard.draw(gRenderer);
+
+                    //Update screen
+                    SDL_RenderPresent( gRenderer );
+                }
+
+            }
+        }
+    }
+
+    //Free resources and close SDL
+    close();
+
     return 0;
 }
